288ea263111b9beee5e56452570ac41473e1661a
Gunni <gunnarthors@gmail.com>
2014-03-08 13:16:38 +0000

    main.html login

--- test/spec/controllers/main.js
+++ test/spec/controllers/main.js
@@ -13,10 +13,10 @@ describe('Controller: MainCtrl', function () {
         scope = $rootScope.$new();
         MainCtrl = $controller('MainCtrl', {
             $scope: scope
-        });
-    }));
+          });
+      }));
 
     it('should attach a list of awesomeThings to the scope', function () {
         expect(scope.awesomeThings.length).toBe(3);
-    });
-});
+      });
+  });
