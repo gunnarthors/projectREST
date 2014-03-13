304d6ad235491dbcc61fbb004f8af75b4f16f907
Gunnar Thor Stefansson <gunnarthors@gmail.com>
2014-03-13 09:43:02 +0000

    changed server url

--- app/scripts/controllers/admin.js
+++ app/scripts/controllers/admin.js
@@ -6,7 +6,7 @@ angular.module('projectRestApp')
         $scope.token = $scope.param.token;
 
         // Get all evaluations
-        BackEnd.authRequest('GET', 'http://project3api.haukurhaf.net/api/v1/evaluations', $scope.token)
+        BackEnd.authRequest('GET', 'http://dispatch.hir.is/h14/api/v1/evaluations', $scope.token)
             .success(function(data){
                 console.log(data);
                 $scope.evaluations = data;
@@ -16,7 +16,7 @@ angular.module('projectRestApp')
             });
 
         // Get all templates
-        BackEnd.authRequest('GET', 'http://project3api.haukurhaf.net/api/v1/evaluationtemplates', $scope.token)
+        BackEnd.authRequest('GET', 'http://dispatch.hir.is/h14/api/v1/evaluationtemplates', $scope.token)
             .success(function(data){
                 console.log(data);
                 $scope.templates = data;
