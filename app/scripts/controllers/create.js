33e72bda37c7fd2c657d0ef45d35a174fb117de4
hildura12 <hildura12@ru.is>
2014-03-13 15:33:09 +0000

    create template

--- app/scripts/controllers/create.js
+++ app/scripts/controllers/create.js
@@ -1,7 +1,48 @@
 'use strict';
 
 angular.module('projectRestApp')
-    .controller('CreateCtrl',['$scope', function ($scope) {
+    .controller('CreateCtrl',['$scope', 'BackEnd', '$route', '$location', function ($scope, BackEnd, $route, $location) {
+        $scope.param = $route.current.params;
+        $scope.token = $scope.param.token;
+        $scope.head = true;
+        $scope.rows = false;
 
-        $scope.temp = 'hundur';
+        $scope.evalObject = {};
+
+        $scope.evalDescript = function(){
+            $scope.evalObject.TitleEN = $scope.title;
+            $scope.evalObject.IntroTextEN = $scope.description;
+            $scope.evalObject.TitleIS = 'doo';
+            $scope.evalObject.IntroTextIS = 'scoopy';
+            $scope.evalObject.CourseQuestions = [];
+            $scope.evalObject.TeacherQuestions = [];
+            $scope.rows = true;
+            console.log($scope.evalObject);
+        };
+
+        $scope.question = {};
+        $scope.question.Answers = [{TextIS: '', TextEN:'', ImageURL: '', Weight: ''},
+            {TextIS: '', TextEN:'', ImageURL: '', Weight: ''}];
+        $scope.addChoice = function() {
+            $scope.question.Answers.push({TextIS: '', TextEN:'', ImageURL: '', Weight: ''});
+            console.log($scope.question);
+        };
+
+        $scope.addCourseQuestion = function() {
+            $scope.evalObject.CourseQuestions.push($scope.question);
+            console.log($scope.evalObject);
+        };
+        $scope.addTeacherQuestion = function() {
+            $scope.evalObject.TeacherQuestions.push($scope.question);
+        };
+        $scope.saveTemplate = function() {
+            BackEnd.authPOST('POST', 'http://dispatch.ru.is/h14/api/v1/evaluationtemplates', $scope.token, $scope.evalObject)
+                .success(function() {
+                    $scope.success = 'Your template has been saved successfully! You will be directed home.';
+                    $location.path('/admin/' + $scope.token);
+                });
+//                .error(function(status) {
+//                    $scope.error.status = status;
+//                });
+        };
     }]);
