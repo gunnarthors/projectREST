34026499eea143809a790ec7dd7b2342aadd80c5
Gunnar Thor Stefansson <gunnarthors@gmail.com>
2014-03-15 09:54:01 +0000

    admin work

--- app/scripts/controllers/admin.js
+++ app/scripts/controllers/admin.js
@@ -1,9 +1,11 @@
 'use strict';
 
 angular.module('projectRestApp')
-    .controller('AdminCtrl',['$scope', '$route', 'BackEnd', '$location', function ($scope, $route, BackEnd, $location) {
+    .controller('AdminCtrl',['$scope', '$route', 'BackEnd', '$location', '$modal', '$log', function ($scope, $route, BackEnd, $location, $modal, $log) {
         $scope.param = $route.current.params;
         $scope.token = $scope.param.token;
+//        $scope.publicSuccess = false;
+//        $scope.publicErr = false;
 
         $scope.createTemplate = function(){
             $location.path('/admin/createTemplate/' + $scope.token);
@@ -22,25 +24,25 @@ angular.module('projectRestApp')
         // Get all templates
         BackEnd.authRequest('GET', 'http://dispatch.ru.is/h14/api/v1/evaluationtemplates', $scope.token)
             .success(function(data){
-                console.log(data);
                 $scope.templates = data;
             })
             .error(function(status){
                 $scope.Eerror = status;
             });
 
+        // Get and show selected template
         $scope.openTemp = function(tempid){
+            $scope.ID = tempid;
             BackEnd.authRequest('GET', 'http://dispatch.ru.is/h14/api/v1/evaluationtemplates/'+ tempid, $scope.token)
             .success(function(data){
-                console.log(data);
                 $scope.evalTemp = data;
+                $scope.currID = tempid;
+                $scope.showTemp = true;
             })
             .error(function(status){
                 $scope.Terror = status;
             });
         };
-
-
 
         // date picker
         $scope.today = function() {
@@ -87,7 +89,7 @@ angular.module('projectRestApp')
         $scope.formats = ['yyyy/MM/dd'];
         $scope.format = $scope.formats[0];
 
-        $scope.formatDate = function(date){
+        $scope.formatDate = function(date) {
             var year = date.getFullYear().toString();
             var month = (date.getMonth()+1).toString();
             if(month.length === 1) {
@@ -98,6 +100,105 @@ angular.module('projectRestApp')
                 day = '0' + day;
             }
             return year + '-' + month + '-' + day;
+        };
+
+        $scope.time = '';
+        // modal stuff
+        $scope.openTime = function () {
+
+            var modalInstance = $modal.open({
+                templateUrl: 'modalTime.html',
+                controller: ModalInstanceCtrl,
+                resolve: {
+                    time: function () {
+                        return $scope.time;
+                    }
+                }
+            });
+
+            modalInstance.result.then(function(time) {
+                $scope.time = time;
+                if($scope.check === true){
+                    $scope.startDate = $scope.startDate + 'T' + $scope.time;
+                }
+                else if($scope.check === false){
+                    $scope.endDate = $scope.endDate + 'T' + $scope.time;
+                }
+            }, function () {
+                $log.info('Modal dismissed at: ' + new Date());
+            });
+        };
+
+        var ModalInstanceCtrl = function ($scope, $modalInstance, time) {
+
+            $scope.time = time;
+            $scope.mytime = new Date();
+            $scope.hstep = 1;
+            $scope.mstep = 1;
+
+            $scope.ismeridian = true;
+            $scope.toggleMode = function() {
+                $scope.ismeridian = ! $scope.ismeridian;
+            };
+
+            $scope.update = function(hour, min) {
+                var d = new Date();
+                d.setHours(hour);
+                d.setMinutes(min);
+                $scope.mytime = d;
+            };
+
+            $scope.formatTime = function(time) {
+                var hour = time.getHours().toString();
+                var min = time.getMinutes().toString();
+                var sec = time.getSeconds().toString();
+                if(hour.length === 1){
+                    hour = '0' + hour;
+                }
+                if(min.length === 1){
+                    min = '0' + min;
+                }
+                if(sec.length === 1){
+                    sec = '0' + sec;
+                }
+                return hour + ':' + min + ':' + sec;
+            };
+
+            $scope.ok = function (mytime) {
+                $scope.time = $scope.formatTime(mytime);
+                $modalInstance.close($scope.time);
+            };
+
+            $scope.cancel = function () {
+                $modalInstance.dismiss('cancel');
+            };
+        };
+
+        $scope.makePublic = function(){
+            if($scope.currID === 'undefined'){
+                console.log('No Template id selected.. weird!.. select template again and try again!');
+            }
+            else{
+                var dataobj = {
+                    TemplateID: $scope.currID,
+                    StartDate: $scope.startDate,
+                    EndDate: $scope.endDate
+                };
+                BackEnd.authPOST('POST', 'http://dispatch.ru.is/h14/api/v1/evaluations',
+                    $scope.token, dataobj)
+                    .success(function(){
+                        console.log('Successfully deployed evaluation ID: ' + $scope.currID + ' Start datetime: ' + $scope.startDate +
+                            ' End datetime: ' + $scope.endDate);
+//                        $location.path('/admin/' + $scope.token);
+                        $scope.publicSuccess = true;
+                        $route.reload();
+                        $scope.publicSuccess = true;
+                    })
+                    .error(function(){
+                        $scope.publicErr = true;
+                        console.log('Evaluation was not deployed... Something went wrong!');
+                    });
+            }
         };
 
 }]);
