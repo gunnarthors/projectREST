31dc3c60ce6b78cc32f850c40edbc3295b798b51
hildura12 <hildura12@ru.is>
2014-03-15 12:31:16 +0000

    student stuff

--- app/scripts/controllers/student.js
+++ app/scripts/controllers/student.js
@@ -3,6 +3,7 @@
 angular.module('projectRestApp.StudentCtrl', [])
     .controller('StudentCtrl',['$scope', 'BackEnd', '$route', function ($scope, BackEnd, $route) {
         $scope.param = $route.current.params;
+        $scope.open = false;
 
           // Get student courses
         BackEnd.authRequest('GET', 'http://project3api.haukurhaf.net/api/v1/my/courses', $scope.param.token)
@@ -21,37 +22,79 @@ angular.module('projectRestApp.StudentCtrl', [])
             $scope.year = time.substr(0,4);
             BackEnd.authRequest('GET', 'http://dispatch.ru.is/h14/api/v1/courses/' + ID + '/' + $scope.year + '/teachers', $scope.param.token)
                 .success(function(data){
-                    console.log(data);
                     $scope.courseTeachers = data;
                     $scope.specialCourse = true;
                 })
                 .error(function(status){
                     $scope.TError = status;
                 });
-//            BackEnd.authRequest('GET', 'http://dispatch.ru.is/h14/api/v1/courses/' + ID + '/' + $scope.year + '/evaluations/1', $scope.param.token)
-//                .success(function(data){                                                                                           //{evalID}
-//                    $scope.courseEvaluation = data;
-//                    console.log(data);
-//                })
-//                .error(function(status){
-//                    $scope.EError = status;
-//                });
+
         };
+
+        $scope.home = function() {
+            $route.reload();
+        };
+
         BackEnd.authRequest('GET', 'http://dispatch.ru.is/h14/api/v1/my/evaluations', $scope.param.token)
             .success(function(data){
-                console.log(data);
                 $scope.evaluations = data;
+                $scope.arr = [];
+                data.forEach(function(entry){
+                    BackEnd.authRequest('GET', 'http://dispatch.ru.is/h14/api/v1/courses/' + entry.CourseID + '/20141/evaluations/' + entry.ID,  $scope.param.token)
+                        .success(function(info){
+                            var obj = entry;
+                            obj.TitleEN = info.TitleEN;
+                            $scope.arr.push(obj);
+
+                        });
+                });
             })
             .error(function(status){
                 $scope.Eerror = status;
             });
-        // Get studen evaluation
-        // server error ??
-//        BackEnd.authRequest('GET', 'http://dispatch.ru.is/h14/api/v1/my/evaluations', $scope.param.token)
-//            .success(function(data){
-//                $scope.evaluations = data;
-//            })
-//            .error(function(status){
-//                $scope.Eerror = status;
-//            });
+        $scope.currentTeacher ='';
+
+        $scope.openEval = function(ID, course) {
+            BackEnd.authRequest('GET', 'http://dispatch.ru.is/h14/api/v1/courses/' + course + '/20141/evaluations/' + ID,  $scope.param.token)
+                .success(function(data){
+                    $scope.open = true;
+                    $scope.openedEvaluation = data;
+                    $scope.currentID = ID;
+                    $scope.currentCourse = course;
+
+                });
+            BackEnd.authRequest('GET', 'http://dispatch.ru.is/h14/api/v1/courses/' + course + '/20141/teachers',  $scope.param.token)
+                .success(function(data){
+                    $scope.currentTeacher = data[0].SSN;
+                });
+        };
+
+        $scope.ansArray = [];
+        $scope.chosen = function(value, ID) {
+            console.log(ID);
+            var ansObj = {
+                QuestionID: ID,
+                TeacherSSN: $scope.currentTeacher,
+                Value: value
+            };
+            if($scope.ansArray.length > 0){
+                $scope.ansArray.forEach(function(data){
+                    if(data.QuestionID === ID ){
+                        var index =  $scope.ansArray.indexOf(data);
+                        if(index > -1) {
+                            $scope.ansArray.splice(index, 1);
+                        }
+                    }
+                });
+            }
+            $scope.ansArray.push(ansObj);
+        };
+        $scope.sendAns = function() {
+            BackEnd.authPOST('POST', 'http://dispatch.ru.is/h14/api/v1/courses/' + $scope.currentCourse + '/20141/evaluations/' + $scope.currentID,
+                    $scope.param.token, $scope.ansArray)
+                .success(function(data){
+                    $route.reload();
+                });
+
+        };
 }]);
