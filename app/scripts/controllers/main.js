5835a5f7014151e897bf7e926e517ef274771681
Gunnar Thor Stefansson <gunnarthors@gmail.com>
2014-03-17 09:42:52 +0000

    unit tests

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -4,6 +4,7 @@
 angular.module('projectRestApp.MainCtrl', [])
     .controller('MainCtrl',['$scope', '$location', 'BackEnd', 'UserFactory', function ($scope, $location, BackEnd, UserFactory) {
         // log in function
+        $scope.usr = {};
         $scope.loginUsr = function() {
             BackEnd.login($scope.usr)
                 .success(function(data) {
@@ -15,14 +16,11 @@ angular.module('projectRestApp.MainCtrl', [])
                         UserFactory.setUserSSN(data.User.SSN);
                         $location.path('/' + data.User.Username + '/' + data.Token);
                     }
-                    else if(data.User.Role === 'admin'){
+                    else {
                         $location.path('/admin/' + data.Token);
                     }
-                    else {
-                        console.log('Something went wrong');
-                    }
                 })
-                .error(function(data, status, headers) {
+                .error(function(status, headers) {
                     $scope.errorMessage = 'Ooops! you entered wrong username or password, please try again!!!';
                     $scope.error = true;
                     $scope.error.header = headers;
--- test/spec/controllers/admin.js
+++ test/spec/controllers/admin.js
@@ -1,3 +1,49 @@
-/**
- * Created by gunnarthors on 17.3.2014.
- */
+'use strict';
+
+describe('projectRestApp.AdminCtrl login', function () {
+    var $httpBackend, $rootScope, createController;
+
+    beforeEach(module('projectRestApp'));
+
+    beforeEach(inject(function($injector){
+
+        $httpBackend = $injector.get('$httpBackend');
+
+        $rootScope = $injector.get('$rootScope');
+
+        var $controller = $injector.get('$controller');
+
+        createController = function(){
+            return $controller('AdminCtrl', {'$scope': $rootScope});
+        };
+    }));
+
+    afterEach(function(){
+        $httpBackend.verifyNoOutstandingExpectation();
+        $httpBackend.verifyNoOutstandingRequest();
+    });
+
+    //on load call to factory
+//    it('should open template', function(){
+//        var controller = createController();
+//
+//        $rootScope.token = 'Z3VubmFyczA5OjEyMzQ=';
+//
+//        BackEnd.authRequest();
+//        $httpBackend.expectGET('http://dispatch.ru.is/h14/api/v1/evaluations', 'Z3VubmFyczA5OjEyMzQ=').respond(201, {data: 'fakedata'});
+//        $httpBackend.flush();
+//    });
+
+
+    //not working yet need to set test first to all load on run first!
+//    it('should open template', function(){
+//        var controller = createController();
+//
+//        $rootScope.token = 'Z3VubmFyczA5OjEyMzQ=';
+//
+//        $rootScope.openTemp(1);
+//        $httpBackend.expectGET('http://dispatch.ru.is/h14/api/v1/evaluationtemplates/1', 'Z3VubmFyczA5OjEyMzQ=').respond(201, {data: 'fakedata'});
+//        $httpBackend.flush();
+//    });
+
+});
